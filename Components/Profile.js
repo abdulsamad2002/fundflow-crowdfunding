2d3ac1a9acae13073cd01e5a5344347a5253df75
Abdul Samad <abdulsamadsjc2002@gmail.com>
2025-11-13 02:11:04 +0530

    Update

--- Components/Profile.js
+++ Components/Profile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@Components/ui/button";
+import { Button } from "@/Components/ui/button";
 
 const Profile = ({ props }) => {
   return (
